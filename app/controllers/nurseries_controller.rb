819679b21edaa243025cc67df6895b1fcfc90760
HIRONO <hilohiro@users.noreply.github.com>
2016-03-04 00:05:29 +0900

    不要な動作を削除.

--- app/controllers/nurseries_controller.rb
+++ app/controllers/nurseries_controller.rb
@@ -1,5 +1,5 @@
 class NurseriesController < ApplicationController
-  before_action :set_nurseries, only: [:show, :edit, :update, :destroy]
+  before_action :set_nurseries, only: [:show]
 
   # GET /nurseries
   # GET /nurseries.json
@@ -10,55 +10,6 @@ class NurseriesController < ApplicationController
   # GET /nurseries/1
   # GET /nurseries/1.json
   def show
-  end
-
-  # GET /nurseries/new
-  def new
-    @nurseries = Nursery.new
-  end
-
-  # GET /nurseries/1/edit
-  def edit
-  end
-
-  # POST /nurseries
-  # POST /nurseries.json
-  def create
-    @nurseries = Nursery.new(nurseries_params)
-
-    respond_to do |format|
-      if @nurseries.save
-        format.html { redirect_to @nurseries, notice: 'Nursery was successfully created.' }
-        format.json { render :show, status: :created, location: @nurseries }
-      else
-        format.html { render :new }
-        format.json { render json: @nurseries.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /nurseries/1
-  # PATCH/PUT /nurseries/1.json
-  def update
-    respond_to do |format|
-      if @nurseries.update(nurseries_params)
-        format.html { redirect_to @nurseries, notice: 'Nursery was successfully updated.' }
-        format.json { render :show, status: :ok, location: @nurseries }
-      else
-        format.html { render :edit }
-        format.json { render json: @nurseries.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /nurseries/1
-  # DELETE /nurseries/1.json
-  def destroy
-    @nurseries.destroy
-    respond_to do |format|
-      format.html { redirect_to nurseries_url, notice: 'Nursery was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
